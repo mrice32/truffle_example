5e63b5c5bc03fc8eb6b927154758a6d8709b4139
Matt Rice <matthewcrice32@gmail.com>
2019-12-27 16:01:30 -0500

    truffle isolation bug example
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -3,9 +3,11 @@ pragma solidity >=0.4.21 <0.7.0;
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
+  uint public abc;
 
   constructor() public {
     owner = msg.sender;
+    abc = 0;
   }
 
   modifier restricted() {
@@ -19,5 +21,9 @@ contract Migrations {
   function upgrade(address new_address) public restricted {
     Migrations upgraded = Migrations(new_address);
     upgraded.setCompleted(last_completed_migration);
+  }
+
+  function setAbc(uint val) public {
+    abc = val;
   }
 }
---
+++ test/1.js
@@ -0,0 +1,10 @@
+const Migrations = artifacts.require("Migrations");
+
+contract("1", function(accounts) {
+  it("Check and Set value", async function() {
+    const migrations = await Migrations.deployed();
+    console.log(migrations.address);
+    assert.equal((await migrations.abc()).toString(), "0");
+    await migrations.setAbc(1);
+  });
+});
